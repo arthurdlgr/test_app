3e2e601312b8c1b0f0ad84d5349d903124537958
arthurdlgr <arthur@alveus.club>
2017-06-24 21:58:52 +0000

    Pages de mises à jour/index/update

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,22 @@
 class ArticlesController < ApplicationController
   
+  def update 
+    
+    @article=Article.find(params[:id])
+    
+    if @article.update(article_params)
+      flash[:notice] = "mis à jour"
+      redirect_to article_path(@article)
+    else
+      render 'edit'
+    end
+  end  
+  
+  def index
+    @articles = Article.all
+  end
+
+  
   def new
     
     @article = Article.new
@@ -9,18 +26,36 @@ class ArticlesController < ApplicationController
   
   def create 
     @article = Article.new(article_params)
-    @article.save
+    if @article.save
+      flash[:notice] = "Article was succesfully created"
+      redirect_to article_path(@article)  
+    else
+      render 'new'
+
+    end
+   
   end
+  
+  def show 
+    
+    @article = Article.find(params[:id])
+    
+  end
+  
+  def edit 
+    
+    @article=Article.find(params[:id])
+    
+  end
+  
+  
+  
     
   private
     
     def article_params
     
     params.require(:article).permit( :title, :description)
-      
-      
+    
     end
-  
-  
-  
-end
+  end     
