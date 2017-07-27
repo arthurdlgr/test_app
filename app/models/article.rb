9dea452fd1e528239c78dcef176262a3851363dd
arthurdlgr <arthur@alveus.club>
2017-07-27 19:51:39 +0000

    user validations et debugger

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,8 +1,6 @@
 class Article < ActiveRecord::Base
-  
+  belongs_to :user
   validates:title, presence:true, length:{minimum:3, maximum:20 }
-  
   validates:description, presence:true, length:{minimum: 5, maximum:50}
-
   
 end
